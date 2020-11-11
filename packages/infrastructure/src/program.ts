118a2f5763ea41b02471e6c7e6e8160f4aba02d8
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2020-11-11 09:30:49 +0100

    change the schedule 10 and 3 CET

--- packages/infrastructure/src/program.ts
+++ packages/infrastructure/src/program.ts
@@ -11,10 +11,7 @@ const program = () => {
   const lambda = botLambda()
 
   // now run the lambda on a schedule
-  // run it at 10am and 15pm on a weekday
-  // const schedule = '0 10,15 ? * MON-FRI *'
-  // bridge('enabler-event', lambda.arn, schedule)
-  bridge('enabler-event', lambda.arn, '0 10,15 ? * MON-FRI *')
+  bridge('enabler-event', lambda.arn, '0 9,14 ? * MON-FRI *')
 }
 
 export default program
